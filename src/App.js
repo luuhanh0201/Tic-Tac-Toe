2006e4a74192cf83fcffc5d6a09347de404beb31
luuhanh <luuhanh0201@gmail.com>
2023-02-27 01:55:30 +0700

    add . files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import Learn from './Learning/UseRef';
+import Learn from './Learning';
 import './globalStyle.scss'
-import { useState } from 'react';
 function App() {
-
-  const [toggle, setToggle] = useState(false)
-
 
   return (
     <div className="App">
-    <button
-    onClick={()=>{setToggle(!toggle)}}
-    >Toggle</button>
-      
-        {toggle &&
-       <Learn/>}
-       
+      <Learn />
     </div>
   );
 }
